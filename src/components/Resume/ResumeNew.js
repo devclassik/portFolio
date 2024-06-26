5c305f91bcfa52777d417e14ed8ab640f2edd3a3
Opemipo <alomajaopemipo8@gmail.com>
2024-06-26 22:02:54 +0100

    new cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Ope_new.pdf";
+import pdf from "../../Assets/../Assets/OpemipoAlomaja.pdf";
 import { AiOutlineDownload, AiOutlineEye } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
